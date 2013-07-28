f15d1543be96d566809a54ec819a373180e7f7c9
wern ancheta <ancheta.wern@gmail.com>
2013-07-28 20:32:39 +0800

    add code for real-time updating of UI for user status and web notifications

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var app_port = 8000;
 var io_port = 3333;
 var express = require('express');
-var app = express();
-var io = require('socket.io').listen(app.listen(io_port));
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+
+var os = require('os');
+
+
 var redis = require("redis");
 var redis_client = redis.createClient();
 
@@ -17,15 +23,14 @@ app.use(express.static('public'));
 var current_users = [];
 
 redis_client.get("wifi_users", function(err, reply){
-	if(reply){
+	if(reply != 'nil'){
 		current_users = JSON.parse(reply);
-		console.log(current_users);
 	}
 });
 
 app.get('/', function(req, res){
+	console.log(req.connection.remoteAddress);
 	res.render('index', {users : current_users});
-	
 });
 
 app.get('/admin', function(req, res){
@@ -37,14 +42,16 @@ app.post('/create_user', function(req, res){
 	var id = current_users.length + 1;
 	var username = req.param('username');
 	var mac_address = req.param('mac_address');
+	var ip_address = req.param('ip_address');
 
 	current_users.push(
 		{
 		'id' : id,
 		'name' : username, 
 		'status' : 'browsing', 
-		'btn_class' : 'btn-primary',
-		'mac_address' : mac_address
+		'mac_address' : mac_address,
+		'ip_address' : ip_address,
+		'btn_class' : 'btn-primary'
 		}
 	);
 
@@ -81,9 +88,31 @@ app.get('/get_users', function(req, res){
 	});
 });
 
+app.get('/db', function(req, res){
+
+	var users = [
+		{'id' : 1, 'name' : 'wern', 'status' : 'browsing', 'mac_address' : '1C:65:9D:3B:1A:C3', 'ip_address' : '192.168.254.102', 'btn_class' : 'btn-primary'},
+		{'id' : 2, 'name' : 'junel', 'status' : 'browsing', 'mac_address' : '20:16:D8:52:CA:D2', 'ip_address' : '192.168.254.106', 'btn_class' : 'btn-primary'},
+		{'id' : 3, 'name' : 'jerome', 'status' : 'browsing', 'mac_address' : '4C:0F:6E:5C:D3:B2', 'ip_address' : '192.168.254.105', 'btn_class' : 'btn-primary'},
+		{'id' : 4, 'name' : 'marvz', 'status' : 'browsing', 'mac_address' : '1C:4B:D6:2D:FC:55', 'ip_address' : '192.168.254.108', 'btn_class' : 'btn-primary'}
+	];
+
+	redis_client.set("wifi_users", JSON.stringify(users));
+});
+
 redis_client.on("error", function(err){
     console.log("Error " + err);
 });
 
 
-app.listen(app_port);
+io.sockets.on('connection', function(socket){
+  
+  socket.on('toggle_status', function(data){
+    io.sockets.emit('update_toggle', data);
+    
+  });
+});
+
+
+
+server.listen(8000);
