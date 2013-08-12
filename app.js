fe53dd4357f0cfb8440fe26d7d8351a2514751c2
wern ancheta <ancheta.wern@gmail.com>
2013-08-12 21:06:52 +0800

    add chat feature

--- app.js
+++ app.js
@@ -29,20 +29,27 @@ redis_client.get("wifi_users", function(err, reply){
 });
 
 app.get('/', function(req, res){
+
+	var current_user_id;
+	var current_user_name;
 	var current_user_count = current_users.length;
+	
 	for(var x = 0; x < current_user_count; x++){
 		current_users[x]['disabled'] = '';
 		if(req.connection.remoteAddress != current_users[x]['ip_address']){
 
 			current_users[x]['disabled'] = 'disabled'; 
+		}else{
+			current_user_id = current_users[x]['id'];
+			current_user_name = current_users[x]['name'];
 		}
 	}
-
-	res.render('index', {users : current_users});
+	
+	res.render('index', {'users' : current_users, 'current_user_id' : current_user_id, 'current_user_name' : current_user_name});
 });
 
 app.get('/admin', function(req, res){
-   res.render('admin', {users : current_users});
+   res.render('admin', {'users' : current_users});
 });
 
 app.post('/create_user', function(req, res){
@@ -102,17 +109,6 @@ app.get('/get_users', function(req, res){
 	});
 });
 
-app.get('/db', function(req, res){
-
-	var users = [
-		{'id' : 1, 'name' : 'wern', 'status' : 'browsing', 'mac_address' : '1C:65:9D:3B:1A:C3', 'ip_address' : '192.168.254.102', 'btn_class' : 'btn-primary'},
-		{'id' : 2, 'name' : 'junel', 'status' : 'browsing', 'mac_address' : '20:16:D8:52:CA:D2', 'ip_address' : '192.168.254.106', 'btn_class' : 'btn-primary'},
-		{'id' : 3, 'name' : 'jerome', 'status' : 'browsing', 'mac_address' : '4C:0F:6E:5C:D3:B2', 'ip_address' : '192.168.254.105', 'btn_class' : 'btn-primary'},
-		{'id' : 4, 'name' : 'marvz', 'status' : 'browsing', 'mac_address' : '1C:4B:D6:2D:FC:55', 'ip_address' : '192.168.254.108', 'btn_class' : 'btn-primary'}
-	];
-
-	redis_client.set("wifi_users", JSON.stringify(users));
-});
 
 redis_client.on("error", function(err){
     console.log("Error " + err);
@@ -124,6 +120,10 @@ io.sockets.on('connection', function(socket){
   socket.on('toggle_status', function(data){
     io.sockets.emit('update_toggle', data);
     
+  });
+
+  socket.on('message', function(data){
+  	io.sockets.emit('send', data);
   });
 });
 
